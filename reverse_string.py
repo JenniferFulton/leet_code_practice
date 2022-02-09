54b44d2f6a70fc7351b26dd588bdcee89f93c48f
JenniferFulton <jennifer.reeves8@gmail.com>
2022-02-08 20:27:29 -0500

    New problems and prompts added

---
+++ reverse_string.py
@@ -0,0 +1,17 @@
+# Write a function that reverses a string. The input string is given as an array of characters s.
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+# Example 1:
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+
+def reverse(list):
+    middle = round(len(list) / 2)
+    for i in range(0, middle, 1):
+        temp = list[i]
+        list[i] = list [len(list)-1-i]
+        list [len(list)-1-i] = temp
+    return list
+
+print(reverse([1,2,3,4,5]))
