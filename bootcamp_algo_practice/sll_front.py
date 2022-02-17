5dbc4bd9fbab82cfcbcabb06ddbe5ada64c01265
JenniferFulton <jennifer.reeves8@gmail.com>
2022-02-17 14:31:40 -0500

    Practice with SLL added.

---
+++ bootcamp_algo_practice/sll_front.py
@@ -0,0 +1,11 @@
+# 1. FRONT
+# Write a method to return the value (not the node) at the head of the list. If the list is empty, return null.
+
+
+# 2. REMOVE FRONT
+# Write a method to remove the head node and return the new list head node. If the list is empty, return null.
+
+
+# 3. ADD FRONT
+# Write a method that accepts a value and create a new node, assign it to the list head, and return a pointer to the new head node.
+
