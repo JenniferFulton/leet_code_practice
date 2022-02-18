c59abbac08d4e3e7fa1e57d1f4a26fecfc02bf3f
JenniferFulton <jennifer.reeves8@gmail.com>
2022-02-18 16:55:30 -0500

    Created add front fucntion, but returning Attribute error, but will not allow me to add data to print statement?

--- bootcamp_algo_practice/sll_front.py
+++ bootcamp_algo_practice/sll_front.py
@@ -18,11 +18,24 @@ class sll():
         
 # 2. REMOVE FRONT
 # Write a method to remove the head node and return the new list head node. If the list is empty, return null.
-
+    def remove_head(self):
+        if self.head == None:
+            print (None)
+        else:
+            removed = self.head
+            self.head = removed.next
+            print(self.head)
+        return self
 
 # 3. ADD FRONT
 # Write a method that accepts a value and create a new node, assign it to the list head, and return a pointer to the new head node.
+    def add_front(self, val):
+        self.next = self.head
+        self.head = val
+        print(self.head)
+        return self
+
 
 
 new_list = sll(7)
-new_list.display_head()
+new_list.add_front(5).display_head()
