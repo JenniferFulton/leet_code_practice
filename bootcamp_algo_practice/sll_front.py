972fa184c805c11f9c67cda0ee8587a26e469ab0
JenniferFulton <jennifer.reeves8@gmail.com>
2022-02-18 13:47:34 -0500

    Attempting to make a method to display the head of the sll but returning a type error

--- bootcamp_algo_practice/sll_front.py
+++ bootcamp_algo_practice/sll_front.py
@@ -9,10 +9,13 @@ class Node():
 class sll():
     def __init__(self, nodeValue):
         self.head = Node(nodeValue)
-
+        return self
 # 1. FRONT
 # Write a method to return the value (not the node) at the head of the list. If the list is empty, return null.
-
+    def display_head(self):
+        print(self.head)
+        return self
+        
 
 # 2. REMOVE FRONT
 # Write a method to remove the head node and return the new list head node. If the list is empty, return null.
@@ -21,3 +24,6 @@ class sll():
 # 3. ADD FRONT
 # Write a method that accepts a value and create a new node, assign it to the list head, and return a pointer to the new head node.
 
+
+new_list = sll(7)
+new_list.display_head()
