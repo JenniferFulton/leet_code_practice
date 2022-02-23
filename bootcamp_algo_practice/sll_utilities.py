c7707014d2653e5a61fc79310df41f9ab0a154b5
JenniferFulton <jennifer.reeves8@gmail.com>
2022-02-23 13:29:59 -0500

    min and max functions added but not working yet

--- bootcamp_algo_practice/sll_utilities.py
+++ bootcamp_algo_practice/sll_utilities.py
@@ -1,11 +1,18 @@
-class Node():
+class Node:
     def __init__(self, value):
         self.value = value
         self.next = None 
 
-class sll():
+class sll:
     def __init__(self, nodeValue):
         self.head = Node(nodeValue)
+    
+    def add_back(self, value):
+        runner = self.head
+        while runner != None:
+            runner = runner.next
+        runner.next = Node(value)
+        return self
 
 # SLL Utilities:
 # Add a method contains(value) to your SLL class, which is given a value as a parameter.  Return a boolean (true/false); true, if the list possesses a node that contains the provided value.
@@ -25,7 +32,7 @@ class sll():
         while runner != None:
             length += 1
             runner = runner.next
-        print(length)
+        print('length is:', length)
         return self
 
 # Display:
@@ -35,16 +42,36 @@ class sll():
         while runner != None:
             print(runner.value)
             runner = runner.next
-
+        return self
 
 # Move:
 # SList: Move Min to Front
-# Create a standalone function that locates the minimum value in a linked list, and moves that node to the front of the list. Return the new list, with all nodes still present, and all (except for the new head node) in their original order.
-
+# Create a standalone function that locates the minimum value in a linked list, and moves that node to the front of the list. 
+# Return the new list, with all nodes still present, and all (except for the new head node) in their original order.
+    def min_toFront(self):
+        minimum = self.head
+        runner = self.head
+        while runner != None:
+            if runner < minimum:
+                minimum = runner
+            runner = runner.next
+        self.head = min
+        return(self)
 
 # SList: Move Max to Back
-# Create a standalone function that locates the maximum value in a linked list, and moves that node to the back of the list. Return the new list, with all nodes still present, and all in their original order except for the node you moved to the end of the singly linked list.
-
+# Create a standalone function that locates the maximum value in a linked list, and moves that node to the back of the list. 
+# Return the new list, with all nodes still present, and all in their original order except for the node you moved to the end of the singly linked list.
+    def mxn_toBack(self):
+            max = self.head
+            runner = self.head
+            while runner != None:
+                if runner > max:
+                    max = runner
+                runner = runner.next
+            runner.next = max
+            return(self)
 
 new_list = sll(7)
-new_list.find_val(7).length().display()
+new_list.head.next = Node(6)
+new_list.head.next.next = Node(3)
+new_list.find_val(6).length().display().min_toFront().display()
