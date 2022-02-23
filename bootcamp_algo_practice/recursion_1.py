2e8d9b1953ecf859484f5f354168ba575909e7bc
JenniferFulton <jennifer.reeves8@gmail.com>
2022-02-23 14:08:41 -0500

    Attempted to solve Sigma however returning RecursionError: maximum recursion depth exceeded in comparison error

--- bootcamp_algo_practice/recursion_1.py
+++ bootcamp_algo_practice/recursion_1.py
@@ -3,18 +3,27 @@
 # 1,1,2,3,5,8,13
 # EX: 6 should return 8 
 
+import re
+
+
 def recursiveFib(int):
     # int is the nth fib number
     if int == 2 or int == 1:
         return 1
     return recursiveFib(int-2) + recursiveFib(int-1)
 
-print(recursiveFib(20))
+# print(recursiveFib(20))
 
 # Recursive Sigma
 # Write a recursive function that given a number returns the sum of integers from 1 to that number. 
 # Example: rSigma(5) = 15 (1+2+3+4+5); rSigma(2.5) = 3 (1+2); rSigma(-1) = 0.
 
+def recursiveSigma(num):
+    if num <= 0:
+        return 0
+    return recursiveSigma(num) + recursiveSigma(num-1)
+
+print(recursiveSigma(5))
 
 # Recursive Factorial
 # Given num, return the product of ints from 1 up to num. If less than zero, treat as zero. 
