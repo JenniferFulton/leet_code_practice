05958578315009b9bbaf96c007d2c5f87002186e
JenniferFulton <jennifer.reeves8@gmail.com>
2022-02-27 11:38:52 -0500

    Recursive Factorial function now working. Both functions made so it can accept a decimal by rounding down to the nearest integer

--- bootcamp_algo_practice/recursion_1.py
+++ bootcamp_algo_practice/recursion_1.py
@@ -14,22 +14,27 @@ def recursiveFib(int):
 # Recursive Sigma
 # Write a recursive function that given a number returns the sum of integers from 1 to that number. 
 # Example: rSigma(5) = 15 (1+2+3+4+5); rSigma(2.5) = 3 (1+2); rSigma(-1) = 0.
+from math import floor
 
 def recursiveSigma(num):
+    rounded_num = floor(num)
     if num <= 0:
         return 0
-    return num + recursiveSigma(num-1)
+    return rounded_num + recursiveSigma(num-1)
 
 print(recursiveSigma(5))
+print(recursiveSigma(2.5))
 
 # Recursive Factorial
 # Given num, return the product of ints from 1 up to num. If less than zero, treat as zero. 
 # If not an integer, truncate. Experts tell us 0! is 1. rFact(3) = 6 (1*2*3). Also, rFact(6.5) = 720 (1*2*3*4*5*6).
 
 def recursiveFact(num):
-    if num <= 0:
-        return 0
-    return num * recursiveFact(num-1)
+    rounded_num = floor(num)
+    if num <= 1:
+        return 1
+    return rounded_num * recursiveFact(num-1)
 
 print(recursiveFact(3))
+print(recursiveFact(6.5))
 
