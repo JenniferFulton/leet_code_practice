dd58623b7e0e3ceb93282ae35ed7ea3221821d17
JenniferFulton <jennifer.reeves8@gmail.com>
2022-03-08 20:19:05 -0500

    accepted by LeetCode

--- math/fizz_buzz.py
+++ math/fizz_buzz.py
@@ -8,15 +8,12 @@
 def fizz_buzz(n):
     array = []
     for i in range(1,n+1,1):
-        if i % 3 == 0:
+        if  i % 3 == 0 and i % 5 == 0:
+            array.append("FizzBuzz")
+        elif i % 3 == 0:
             array.append("Fizz")
-        
         elif i % 5 == 0:
             array.append("Buzz")
-        
-        elif  i % 3 == 0 and i % 5 == 0:
-            array.append("FizzBuzz")
-        
         else:
             array.append(str(i))
         
