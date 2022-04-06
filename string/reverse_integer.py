09918e795ef22c3d890c918be9a93def69e00e57
JenniferFulton <jennifer.reeves8@gmail.com>
2022-04-06 10:22:49 -0400

    solved and accepted

--- string/reverse_integer.py
+++ string/reverse_integer.py
@@ -10,28 +10,56 @@
 
 
 def reverse_int(x):
-    # num = str(x)
-    # return(int(num[::-1]))
-
+    if x == 0:
+        return 0
+    #if it is a negative number
     if x < 0:
         neg_num = str(x * -1)
-        length = len(neg_num)
-        middle = round(length/2)
+        list_negNum = list(neg_num)
+        
+        #reverse the list
+        middle = len(list_negNum)//2
         for i in range(middle):
-            neg_num.replace(neg_num[i],neg_num[length-1-i]) 
-            neg_num.replace(neg_num[length-1-i],num[i])
-        return(int(neg_num * -1))
+            temp = list_negNum[i]
+            list_negNum[i] = list_negNum[len(list_negNum)-1-i]
+            list_negNum[len(list_negNum)-1-i] = temp
+        
+        i = 0
+        while i <= len(list_negNum)-1:
+            if list_negNum[i]=="0":
+                list_negNum.pop(i)
+            else:
+                break
+                
+        new_num = ''.join(list_negNum)
+        x = int(new_num) * -1
+        
+        if int(x) < -2**31 or int(x) > 2**31-1:
+            return 0
+        
+        return x
+    
     else:
         num = str(x)
-        length = len(num)
-        middle = round(length/2)
+        list_num = list(num)
+        middle = len(list_num)//2
         for i in range(middle):
-            temp = num[i]
-            num = num.replace(num[i],num[length-1-i]) 
-            num = num.replace(num[length-1-i],temp)
-        return(int(num))
+            temp = list_num[i]
+            list_num[i] = list_num[len(list_num)-1-i]
+            list_num[len(list_num)-1-i] = temp
+        
+        i = 0
+        while i <= len(list_num)-1:
+            if list_num[0]=="0":
+                list_num.pop(i)
+            else:
+                break
+                
+        x = ''.join(list_num)
+        
+    if int(x) < -2**31 or int(x) > 2**31:
+        return 0
     
-print(reverse_int(1234))
-
-string = "Jennifer"
-print(string.replace("f","B",1))
+    return x
+        
+            
