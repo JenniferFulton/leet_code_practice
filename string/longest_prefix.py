9abe57fdcceee8063cebbfe96475c01d484ad5be
JenniferFulton <jennifer.reeves8@gmail.com>
2022-04-08 16:10:27 -0400

    needs a breaking point if they do not match

--- string/longest_prefix.py
+++ string/longest_prefix.py
@@ -6,20 +6,36 @@
 # Output: "fl"
 
 def longestCommonPrefix(strs):
-        common_prefix = ""
-        shortest = len(strs[0])
-        
-        #finding the shortest string in list
-        for i in range(len(strs)):
-            if len(strs[i]) < shortest:
-                shortest = len(strs[i])
+    common_prefix = ""
+    shortest = len(strs[0])
+    arr = []
+    
+    if len(strs) == 1:
+        return(strs[0])
+    
+    #finding the shortest string in list
+    for i in range(len(strs)):
+        if len(strs[i]) < shortest:
+            shortest = len(strs[i])
+            
+    # adding all the characters(number of the shortest string) to the array
+    for i in range(shortest):
+        arr.append(strs[0][i])
+    # print(arr)
+    
+    # comparing values in the array to characters within the strings
+    i = 0
+    while i <= len(arr)-1:
+        for j in range(len(strs)-1):
+            if arr[i] == strs[j][j] and arr[i] == strs[j+1][j]:
+                common_prefix = common_prefix + arr[i]
+            # else:
+            #     break
+        i += 1
 
-        i = 0
-        while i <= len(strs):
-            for j in range(shortest):
-                print(strs[i][j])
-                # if strs[i][j] == strs[i+1][j]:
-                #     common_prefix = common_prefix + strs[i][j]
-            i += 1
+                    
+    return common_prefix
 
-longestCommonPrefix(["flower","flow","flight"])
+print(longestCommonPrefix(["flower","flow","flight"]))
+print(longestCommonPrefix(["ab", "a"]))
+print(longestCommonPrefix(["reflower","flow","flight"]))
