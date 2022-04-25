082a02b5a26736608981b60eb38273a0830a2809
JenniferFulton <jennifer.reeves8@gmail.com>
2022-04-25 14:33:27 -0400

    not shifting properly

--- arrays/duplicate_zeros.py
+++ arrays/duplicate_zeros.py
@@ -8,12 +8,19 @@
 # Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
 
 def duplicateZeros(arr):
-    for i in range (len(arr)-1):
+    i = 0
+    while i < len(arr)-1:
         if arr[i] == 0:
-            for j in range(i,len(arr)-1,1):
+            for j in range(i+1,len(arr)-1,1):
+                temp = arr[j+1]
                 arr[j+1] = arr[j]
+                arr[j+2] = temp
+                break
             arr[i+1] = 0
-        
+            i += 2
+        else:
+            i += 1
+    return arr
 
 print(duplicateZeros([1,0,2,3,0,4,5,0]))
     
