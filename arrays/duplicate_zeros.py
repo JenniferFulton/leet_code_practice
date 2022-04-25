7b3b0c2fb477e30140220cca47ecc68a5471dfbf
JenniferFulton <jennifer.reeves8@gmail.com>
2022-04-25 13:40:42 -0400

    current solution makes itso all numbers after the first 0 will be zero because you create a new zero

--- arrays/duplicate_zeros.py
+++ arrays/duplicate_zeros.py
@@ -6,3 +6,14 @@
 # Input: arr = [1,0,2,3,0,4,5,0]
 # Output: [1,0,0,2,3,0,0,4]
 # Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+
+def duplicateZeros(arr):
+    for i in range (len(arr)-1):
+        if arr[i] == 0:
+            for j in range(i,len(arr)-1,1):
+                arr[j+1] = arr[j]
+            arr[i+1] = 0
+        
+
+print(duplicateZeros([1,0,2,3,0,4,5,0]))
+    
