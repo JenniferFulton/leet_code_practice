970e8c0efeedc3c375ff1f467a7077f44b1c8f0b
JenniferFulton <jennifer.reeves8@gmail.com>
2022-04-25 13:11:20 -0400

    solved

--- arrays/squares.py
+++ arrays/squares.py
@@ -5,3 +5,16 @@
 # Output: [0,1,9,16,100]
 # Explanation: After squaring, the array becomes [16,1,0,9,100].
 # After sorting, it becomes [0,1,9,16,100].
+
+from tkinter import Place
+
+
+def sortedSquares(nums):
+    for i in range(len(nums)):
+        nums[i] = nums[i]**2
+    nums.sort()
+    return (nums)
+
+# O(1) space compexity
+#     modified in Place
+# sort method makes time compxity O(logN)
