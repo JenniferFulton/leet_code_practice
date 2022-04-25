3bc5507d2cefc2c4ef8ecd2ed15be3e86d16a27f
JenniferFulton <jennifer.reeves8@gmail.com>
2022-04-25 12:32:33 -0400

    solved

--- arrays/consecutive_ones.py
+++ arrays/consecutive_ones.py
@@ -3,3 +3,21 @@
 # Example 1:
 # Input: nums = [1,1,0,1,1,1]
 # Output: 3
+
+def findMaxConsecutiveOnes(nums):
+    cur_max = 0
+    new_max = 0
+    for i in nums:
+        if i == 1:
+            new_max += 1
+        else:
+            if new_max > cur_max:
+                cur_max = new_max
+            new_max = 0
+        if new_max > cur_max:
+                cur_max = new_max
+    return(cur_max)
+
+print(findMaxConsecutiveOnes([1,1,0,1,1,1]))
+
+# O(N) time complexity
