996e91800adaaa703503bcfd9323e556abebb740
JenniferFulton <jennifer.reeves8@gmail.com>
2022-04-26 11:49:02 -0400

    assignment prompt and BigO notation added

--- arrays/remove_element.py
+++ arrays/remove_element.py
@@ -1,18 +1,31 @@
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+# Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+# Return k after placing the final result in the first k slots of nums
+# Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+# EX:
+# Input: nums = [0,1,2,2,3,0,4,2], val = 2
+# Output: 5, nums = [0,1,4,0,3,_,_,_]
+# Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+# Note that the five elements can be returned in any order.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+
 def removeElement(nums, val):
     i = 0
-    length = len(nums)
-    while i < length:
+    k = len(nums)
+    while i < k:
         if nums[i] == val:
             for j in range(i+1, len(nums),1):
                 nums[j-1] = nums[j]
-            length -= 1
+            k -= 1
             i = 0
         else:
             i += 1
-        # else:
-        #     i += 1
-    return nums
+    return k
 
 print(removeElement([0,1,2,2,3,0,4,2],2))
+
+# space complexity O(1)
+# time complexity O(N^2)?
 
 
