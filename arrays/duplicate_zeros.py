c69fa6bea72c70877d46817e9b8327a03b256d82
JenniferFulton <jennifer.reeves8@gmail.com>
2022-04-26 10:56:40 -0400

    functioning for example tested

--- arrays/duplicate_zeros.py
+++ arrays/duplicate_zeros.py
@@ -11,12 +11,8 @@ def duplicateZeros(arr):
     i = 0
     while i < len(arr)-1:
         if arr[i] == 0:
-            for j in range(i+1,len(arr)-1,1):
-                temp = arr[j+1]
-                arr[j+1] = arr[j]
-                arr[j+2] = temp
-                break
-            arr[i+1] = 0
+            for j in range(len(arr)-1,i,-1):
+                arr[j] = arr[j-1]
             i += 2
         else:
             i += 1
