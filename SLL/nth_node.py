480f31fd243ef0101bf12e7bac88b769b07e03ef
JenniferFulton <jennifer.reeves8@gmail.com>
2022-05-10 11:30:49 -0400

    able to go through the list and pull out each value

--- SLL/nth_node.py
+++ SLL/nth_node.py
@@ -9,10 +9,14 @@
 # Output: []
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
     def removeNthFromEnd(self, head, n):
+        runner = head
+        while runner != None:
+            print(runner.val)
+            runner = runner.next
