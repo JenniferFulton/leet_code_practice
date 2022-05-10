a572bc8f0a002d379524f9b8aefb4a247474ca82
JenniferFulton <jennifer.reeves8@gmail.com>
2022-05-10 11:29:34 -0400

    prompt added

--- SLL/nth_node.py
+++ SLL/nth_node.py
@@ -1,41 +1,18 @@
-def solution(numbers, left, right):
-    array = []
-    for i in range(len(numbers)):
-        # compute x=numbers[i]/(i+1)
-        # if left <= x <= right then array[i]=true, else array[i] = false
-        x = float(numbers[i])/float((i+1))
-        print(x)
-        if x >= left and x <= right:
-            # numbers[i] == True
-            array.append(True)
-        else:
-            # numbers[i] == False
-            array.append(False)
-        
-    return array
+# Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
-print(solution([8, 5, 6, 16, 5], 1, 3))
+# EX 1:
+# Input: head = [1,2,3,4,5], n = 2
+# Output: [1,2,3,5]
 
-# Input:
-# numbers: [8, 5, 6, 16, 5]
-# left: 1
-# right: 3
+# EX 2:
+# Input: head = [1], n = 1
+# Output: []
 
-# Input:
-# numbers: [65, 46, 60, 164, 243, 228, 231, 298, 231, 211]
-# left: 20
-# right: 50
-# Output:
-# [false, true, true, true, true, true, true, true, true, true]
-# Expected Output:
-# [false, true, true, true, false, true, true, false, false, false]
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
-# Only solved 1/16 hidden tests
-
-
-
-
-sentence = "the bird and the dog are loud"
-words = ['cat', 'dog']
-
-[5,2]
+class Solution:
+    def removeNthFromEnd(self, head, n):
