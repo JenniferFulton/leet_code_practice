cdf8a069604ec16271d13ae54a2e855fcd61f911
JenniferFulton <jennifer.reeves8@gmail.com>
2022-05-16 12:38:44 -0400

    restarting. able to handle if len of array is 1. Identifies the shortest string within the array

--- string/longest_prefix.py
+++ string/longest_prefix.py
@@ -8,7 +8,6 @@
 def longestCommonPrefix(strs):
     common_prefix = ""
     shortest = len(strs[0])
-    arr = []
     
     if len(strs) == 1:
         return(strs[0])
@@ -17,34 +16,11 @@ def longestCommonPrefix(strs):
     for i in range(len(strs)):
         if len(strs[i]) < shortest:
             shortest = len(strs[i])
-            
-    # adding all the characters(number of the shortest string) to the array
-    for i in range(shortest):
-        arr.append(strs[0][i])
-    # print(arr)
-    
-    # comparing values in the array to characters within the strings
-    i = 0
-    # while loop with go the length of arr to account for every character
-    while i <= len(arr)-1:
-        #for loop will account for every word
-        for j in range(len(strs)-1):
-            # if the character in the arr doesn't equal the character in the string(strs[i])
-            # and the string after it return common prefix
-            current_char = arr[i]
-            current_string = strs[j]
-            if arr[i] == strs[j][i]:
-                common_prefix = common_prefix + arr[i]
-            else:
-                # if they are all equal, add it to the common prefix
-                return(common_prefix)
-                
-            i += 1
 
-                    
     return common_prefix
 
-print(longestCommonPrefix(["flower","flow","flight"]))
-print(longestCommonPrefix(["ab", "a"]))
-print(longestCommonPrefix(["reflower","flow","flight"]))
-print(longestCommonPrefix(["flower","flower","flower","flower"]))
+# print(longestCommonPrefix(["flower","flow","flight"]))
+# print(longestCommonPrefix(["ab", "a"]))
+# print(longestCommonPrefix(["reflower","flow","flight"]))
+# print(longestCommonPrefix(["flower","flower","flower","flower"]))
+print(longestCommonPrefix(["sunflower"]))
